c3057a460d04d9bf273d0a7b8b1c53bd5151b721
Yura <polyachenko.yua@phystech.edu>
2020-03-02 19:07:18 +0300

    walls 1st attempt. Stupid way - just putting u=0 and v=0 where necessary. It blows up.

--- step_center_walls.m
+++ step_center_walls.m
@@ -4,16 +4,11 @@ function [u, v, h] = step_center_walls(g, H, u_prev, v_prev, h_prev, dx, dy, dt)
     dv = -(g * dt * 2) * diff_center_periodic(h_prev, [1, 0], dy);
     dh = -(H * dt * 2) * (diff_center_periodic(u_prev, [0, 1], dx) + ...
                           diff_center_periodic(v_prev, [1, 0], dy)); % wors OK
-    
-    du(:, 1) = -(g * dt * 2) * (h_prev(:, 2) - h_prev(:    , N    )) / (dx * 2);
-    du(:, N) = -(g * dt * 2) * (h_prev(:, 1) - h_prev(:    , N - 1)) / (dx * 2);
-    dv(1, :) = -(g * dt * 2) * (h_prev(2, :) - h_prev(N    , :    )) / (dy * 2);
-    dv(N, :) = -(g * dt * 2) * (h_prev(1, :) - h_prev(N - 1, :    )) / (dy * 2);    
-    
+        
     u = u_prev + du;
     v = v_prev + dv;
     h = h_prev + dh;
     
-    u(:, 1) = 0; u(:, end) = 0;
-    v(1, :) = 0; v(end, :) = 0;    
+    u(:, 1) = 0; u(:, N) = 0;
+    v(1, :) = 0; v(N, :) = 0;    
 end
--- step_fwd_walls.m
+++ step_fwd_walls.m
@@ -1,20 +1,14 @@
 function [u, v, h] = step_fwd_walls(g, H, u_prev, v_prev, h_prev, dx, dy, dt)
-    du = zeros(size(u_prev));
-    dv = zeros(size(v_prev));
-    dh = zeros(size(h_prev));
-    
-    du(:, 2:(end - 1)) = -(g * dt) * diff_fwd_walls(h_prev, [0, 1], dx);
-    %du(:, 1) = 0; du(:, end) = 0;
-    dv(2:(end - 1), :) = -(g * dt) * diff_fwd_walls(h_prev, [1, 0], dy);
-    %dv(1, :) = 0; dv(end, :) = 0;
-    dh(:, 2:(end - 1)) = diff_fwd_walls(u_prev, [0, 1], dx);
-    dh(2:(end - 1), :) = dh(2:(end - 1), :) + diff_fwd_walls(v_prev, [1, 0], dy);
-    dh = -(H * dt) * dh;
+    N = size(h_prev, 1);
+    du = -(g * dt) * diff_center_periodic(h_prev, [0, 1], dx);
+    dv = -(g * dt) * diff_center_periodic(h_prev, [1, 0], dy);
+    dh = -(H * dt) * (diff_center_periodic(u_prev, [0, 1], dx) + ...
+                      diff_center_periodic(v_prev, [1, 0], dy)); % wors OK
         
     u = u_prev + du;
     v = v_prev + dv;
-    h = h_prev + dh;
+    h = h_prev + dh;    
     
-    u(:, 1) = 0; u(:, end) = 0;
-    v(1, :) = 0; v(end, :) = 0;
+    u(:, 1) = 0; u(:, N) = 0;
+    v(1, :) = 0; v(N, :) = 0;
 end
