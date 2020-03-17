2e9b38f4822416ff93d08e050842ff4f3a6e7aa7
polyachenkoya <polyachenkoya@gmail.com>
2020-03-17 10:50:14 +0300

    explicit dh boundary conds done. still doesn't work

--- diff_center_walls.m
+++ diff_center_walls.m
@@ -1,4 +1,5 @@
 function d = diff_center_walls(f, shft, dx)
+% isn't currently used
     d = (f((1 + 2*shft(1)) : (end            ), (1 + 2*shft(2)) : (end            )) - ...
          f((1            ) : (end - 2*shft(1)), (1            ) : (end - 2*shft(2)))) / (dx * 2);         
 end
--- step_center_walls.m
+++ step_center_walls.m
@@ -1,14 +1,15 @@
 function [u, v, h] = step_center_walls(g, H, u_prev, v_prev, h_prev, dx, dy, dt)
-    N = size(h_prev, 1);
     du = -(g * dt * 2) * diff_center_periodic(h_prev, [0, 1], dx);
     dv = -(g * dt * 2) * diff_center_periodic(h_prev, [1, 0], dy);
     dh = -(H * dt * 2) * (diff_center_periodic(u_prev, [0, 1], dx) + ...
                           diff_center_periodic(v_prev, [1, 0], dy)); % wors OK
-        
+                      
+    dh(1, :) = 0; dh(end, :) = 0;
+    dh(:, 1) = 0; dh(:, end) = 0;
     u = u_prev + du;
     v = v_prev + dv;
     h = h_prev + dh;
     
-    u(:, 1) = 0; u(:, N) = 0;
-    v(1, :) = 0; v(N, :) = 0;    
+    u(:, 1) = 0; u(:, end) = 0;
+    v(1, :) = 0; v(end, :) = 0;    
 end
